f5992c2e081152445f30a0fed8d45c99040daa2c
tailsjs <49571184+tailsjs@users.noreply.github.com>
2020-06-01 19:22:26 +0300

    1.0.9.1 - Coronavirus Update
    обновил источник

--- getInfoCoronaVirus.js
+++ getInfoCoronaVirus.js
@@ -1,11 +1,17 @@
 async function genius(){
-	let result = ((await fetch('https://api.covid19api.com/summary')).json())
+	let result = ((await fetch('https://api.thevirustracker.com/free-api?global=stats')).json())
 	 a = await result,
 	 inf = document.getElementById('infected'),
 	heal = document.getElementById('healed'),
-	death = document.getElementById('dead')
-	inf.innerHTML = a.Global.TotalConfirmed + ` (новых заражений: ${a.Global.NewConfirmed})`
-	heal.innerHTML = a.Global.TotalRecovered + ` (новых спасений: ${a.Global.NewRecovered})`
-	death.innerHTML = a.Global.TotalDeaths + ` (новых смертей: ${a.Global.NewDeaths})`
+	death = document.getElementById('dead'),
+	active = document.getElementById('act'),
+	serious = document.getElementById('ser'),
+	eff = document.getElementById('str')
+	inf.innerHTML = a.results[0].total_cases + ` (новых заражений: ${a.results[0].total_new_cases_today})`
+	heal.innerHTML = a.results[0].total_recovered
+	death.innerHTML = a.results[0].total_deaths + ` (новых смертей: ${a.results[0].total_new_deaths_today})`
+	active.innerHTML = a.results[0].total_active_cases
+	serious.innerHTML = a.results[0].total_serious_cases
+	eff.innerHTML = a.results[0].total_affected_countries
 }
 window.onload = genius
